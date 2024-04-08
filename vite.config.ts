6382ca4172946da52aee00e4b351c012b590700c
artem-kuznecov <artemkuz.wmg@gmail.xn--co-nnc>
2024-04-08 20:40:14 +0300

    Add global scss variables file

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,15 @@ export default defineConfig({
   plugins: [
     vue()
   ],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @import "./src/assets/_variables.scss";
+        `
+      }
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
