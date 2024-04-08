effb0e5429d904d24128af8e4db669fbf2e38091
artem-kuznecov <artemkuz.wmg@gmail.xn--co-nnc>
2024-04-08 20:37:40 +0300

    Add floors grid

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,18 @@
 <script setup lang="ts">
+  import FloorsGrid from '@/components/FloorsGrid.vue'
+  import { settingsStore } from '@/stores/settings'
+
+  const settings = settingsStore()
+  const { addFloors, reduceFloors } = settings
+
 </script>
 
 <template>
-  dummy
+  <!-- DEV -->
+  <button style="position: absolute; top: 0; z-index: 100;" @click="addFloors()">add floor</button>
+  <button style="position: absolute; top: 25px; z-index: 100;" @click="reduceFloors()">reduce floor</button>
+  <!-- DEV -->
+  <FloorsGrid />
 </template>
 
 <style scoped>
---
+++ src/components/FloorsGrid.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+  import { storeToRefs } from 'pinia'
+  import { computed } from 'vue'
+
+  import { settingsStore } from '@/stores/settings'
+
+  const settings = settingsStore()
+
+  const { floors } = storeToRefs(settings)
+
+  const floorHeight = computed(() => {
+    return {
+      'height': 100 / floors.value + '%'
+    }
+  })
+</script>
+
+<template>
+  <section>
+    <div v-for="floor in floors" v-bind:key="floor" :style="floorHeight"></div>
+  </section>
+</template>
+
+<style scoped lang="scss">
+  section {
+    height: 100%;
+
+    div {
+      border-bottom: 1px solid black;
+    }
+  }
+</style>
