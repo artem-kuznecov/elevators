07459b8dd93a9ad193d65f4ebd486d7914d2a21f
artem-kuznecov <artemkuz.wmg@gmail.xn--co-nnc>
2024-04-10 13:16:10 +0300

    add dev needed buttons

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,27 @@
 <script setup lang="ts">
   import FloorsGrid from '@/components/FloorsGrid.vue'
+  import ShaftsGrid from '@/components/shafts/ShaftsGrid.vue'
   import { settingsStore } from '@/stores/settings'
+  import { storeToRefs } from 'pinia'
 
   const settings = settingsStore()
-  const { addFloors, reduceFloors } = settings
+  const { shafts, processingCabins } = storeToRefs(settings)
+  const { addFloors, reduceFloors, addShafts, reduceShafts, processQueue } = settings
 
 </script>
 
 <template>
   <!-- DEV -->
-  <button style="position: absolute; top: 0; z-index: 100;" @click="addFloors()">add floor</button>
-  <button style="position: absolute; top: 25px; z-index: 100;" @click="reduceFloors()">reduce floor</button>
+  <button style="position: absolute; top: 0; right: 0; z-index: 100;" @click="addFloors()">add floor</button>
+  <button style="position: absolute; top: 25px; right: 0; z-index: 100;" @click="reduceFloors()">reduce floor</button>
+
+  <button style="position: absolute; top: 50px; right: 0; z-index: 100;" @click="addShafts()">add shaft</button>
+  <button style="position: absolute; top: 75px; right: 0; z-index: 100;" @click="reduceShafts()">reduce shaft {{ shafts.length }}</button>
+  <button style="position: absolute; top: 100px; right: 0; z-index: 100;" @click="processQueue()">[{{ processingCabins }}]proccess from 2</button>
+  <p style="position: absolute; top: 140px; right: 0; z-index: 100;">{{ shafts }}</p>
   <!-- DEV -->
   <FloorsGrid />
+  <ShaftsGrid />
 </template>
 
 <style scoped>
